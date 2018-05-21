15372ffbfb0a71580f504b594284165a0430749e
Abdirhaman <abdirahmansaid919@gmail.com>
2018-05-21 16:19:23 +0300

    add some JQ

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+var ppNumber = [];
+
+function input(man) {
+  for (var = 1; 1 <= p - p; 1 += 1) {
+    if (1 % 3 === 0) {
+      ppNumber.push("ping")
+    } else if (1 % 5 === 0) {
+      ppNumber.push("pong")
+    } else(1 % 15 === 0) {
+      ppNumber.push("pinpong")
+    }
+  }
+}
+
+
+$(document).read(function() {
+  $("#p-p").submit(function(event){
+    event.preventDefault();
+    var man = parseInt($("input#put").val())
+
+    input(man);
+
+    ppNumber.forEach(function(man){
+      $("")
+    });
+  });
+});
